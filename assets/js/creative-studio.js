04bcaff3d3ad05f68ea7da9ad62569e987df09cf
Haru <134833706+Haroon3871@users.noreply.github.com>
2023-12-22 18:12:17 +0500

    Pluto

--- assets/js/creative-studio.js
+++ assets/js/creative-studio.js
@@ -16,3 +16,14 @@ $(document).ready(function(){
     });
 }); 
 
+
+// Wait for the website to finish loading
+window.addEventListener('load', function() {
+    const loaderWrapper = document.querySelector('.loader-wrapper');
+    const content = document.querySelector('.content');
+  
+    // Hide the loader and show the content after the website finishes loading
+    loaderWrapper.style.display = 'none'; // Hide the loader
+    content.style.display = 'block'; // Show the content
+  });
+  
